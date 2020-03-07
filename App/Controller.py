e0320b9c049b2e94926a3a27ebacf1f68e0259a5
NazGy <nazargy@gmail.com>
2020-03-07 17:12:36 -0500

    controller + definitions

--- App/Controller.py
+++ App/Controller.py
@@ -1,10 +1,24 @@
 from flask import Flask
+from flask import request
+
+from audioParser import *
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello, World!"
+@app.route("/getQuestions", methods=['GET'])
+def getQuestions():
+    print("HI")
+    return questionsID()
+
+@app.route("/qID/<id>", methods=['GET'])
+def questionID(id):
+    return question(id)
+
+@app.route("/audio", methods=['POST'])
+def postAudio():
+    response = request.get_json()
+    score = audio(response)
+    return score
     
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ App/main.py
@@ -0,0 +1,39 @@
+from os import path
+import json
+
+
+
+def question(qID):
+    # This function returns an audio file!
+    return "What are your weaknesses?"
+
+def questionsID():
+    # Randomly picks 5 questions?
+    return {"qId": ["1", "4"]}
+
+def audio(body):
+    with open(path.join(path.dirname(__file__), "SampleText.json"), 'r') as json_file:
+        data = json.load(json_file)
+        assessText(data["Questions"][0])
+    return "Hello World!"
+
+
+# Given a question object which inclues:
+# Question
+# Answer
+# Keywords
+# Time
+def assessText(json):
+    # print(json)
+    question = json["Question"]
+    keywords = map(lambda keyword: keyword.lower(), json["Keywords"].split(","))
+    time = json["Time"]
+    answer = json["Answer"].lower()
+
+    umCount = answer.count("um")
+    likeCount = answer.count("like")
+
+    print("Ums:", umCount)
+    print("likes:", likeCount)
+
+
