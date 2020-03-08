e7628cf391386250d7572874db7e4ae7997e79d9
Maria Esipova <mariaesipova@revyoo.me>
2020-03-08 06:32:49 -0400

    added a var to Feedback

--- my_interviewer_app/src/Feedback.js
+++ my_interviewer_app/src/Feedback.js
@@ -5,9 +5,11 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'; // possibly not
 
 export class Feedback extends React.Component {
     render() {
+        var feedbackId = this.props.match.params.feedbackNum
+        var nextFeedbackLink= '/question/'+ (parseInt(feedbackId,10)+1)
         return (
             <div className="Feedback">
-            <h1>Feedback page {this.props.match.params.feedbackNum}</h1>
+            <h1>Feedback page {feedbackId}</h1>
             <h2>I will be giving you feedback</h2>
             </div>
         );
