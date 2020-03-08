d7e0def59d45a1d64fd7f84378f53d0a8e5dfcdf
Maria Esipova <mariaesipova@revyoo.me>
2020-03-08 10:08:45 -0400

    got audio recorder working in questions

--- my_interviewer_app/src/Constants.js
+++ my_interviewer_app/src/Constants.js
@@ -1,5 +1,9 @@
 import UserInfo from "./UserInfo"
 
+const Constants = {}
+
 const numOfInterviewQuestionsToAsk = 3
 
 var user = new UserInfo("", 0, 0)
+
+export default Constants
--- my_interviewer_app/src/Question.js
+++ my_interviewer_app/src/Question.js
@@ -4,10 +4,43 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'; // possibly not
 //import { render } from '@testing-library/react';
 import { withRouter } from "react-router";
 
+import { ReactMic } from 'react-mic';
+
+
 import Constants from './Constants'
 
 
 export class Question extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          record: false
+        }
+     
+      }
+
+
+      startRecording = () => {
+        this.setState({
+          record: true
+        });
+      }
+     
+      stopRecording = () => {
+        this.setState({
+          record: false
+        });
+      }
+     
+      onData(recordedBlob) {
+        console.log('chunk of real-time data is: ', recordedBlob);
+      }
+     
+      onStop(recordedBlob) {
+        console.log('recordedBlob is: ', recordedBlob);
+      }
+
 
     render(){
 
@@ -17,7 +50,7 @@ export class Question extends React.Component {
     return (
         <div className="Question">
             <h1>Question page {questionId} </h1>
-            <h2>I will be reading the question</h2>
+            <h2>Press button to read the question:</h2>
             <button 
                 title = "play sound"
                 type = "button"
@@ -29,6 +62,27 @@ export class Question extends React.Component {
                     Play Audio
              </button>
              <br></br>
+
+            <div>
+             <ReactMic
+                record={this.state.record}
+                className="sound-wave"
+                onStop={this.onStop}
+                onData={this.onData}
+                strokeColor="#000000"
+                backgroundColor="#FF4081" />
+                <button onClick={this.startRecording} 
+                type="button"
+                className = "action-button">
+                    Start
+                </button>
+                <button onClick={this.stopRecording} 
+                type="button"
+                className = "action-button">>
+                    Stop
+                </button>
+            </div>
+
             <button
                 title = "next-question"
                 type = "button"
