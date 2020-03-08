2c0eae0ecd38df193552879a01d995c2862bcd1f
Maria Esipova <mariaesipova@revyoo.me>
2020-03-07 19:57:18 -0500

    prior to attempting v1

--- my_interviewer_app/src/App.js
+++ my_interviewer_app/src/App.js
@@ -1,33 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+//import logo from './logo.svg';
 import './App.css';
+import { render } from '@testing-library/react';
 
 function App() {
+
+  var [counter, setCounter] = useState(0)
+
+  useEffect(() => {
+    console.log("page has rerendered");
+    // make request from API
+  }, [counter]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Title</p>
-        <p>Information</p>
-        <a
-          className="App-link"
-          href="https://google.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          >Click here
-          </a>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and code.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className = "search-form">
+        <input className = "search-bar" type="text"/>
+        <button className = "search-button" type="submit">
+          Search
+          </button>
+      </form>
+      <h1 onClick={() => setCounter(counter+1)}>
+        {counter}
+        </h1>
+      <h2>subheader?</h2>
+      <body>body?</body>
     </div>
   );
 }
